91ce659e9d15ea1dd4a2cdfb67dc2cd5bf9f1ae0
gyuhaaa <57971392+gyuhaaa@users.noreply.github.com>
2021-06-01 23:53:13 +0900

    테이블에 trigger 걸기

---
+++ trigger.sql
@@ -0,0 +1,51 @@
+CREATE TRIGGER TRIGGER_NAME
+	ON TRIGGER_TABLE
+	AFTER INSERT, DELETE, UPDATE
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+
+    DECLARE
+        @ACTION CHAR(1)
+
+    SET @ACTION = 'I'
+
+    IF EXISTS(SELECT * FROM DELETED)
+    BEGIN
+        SET @ACTION = (CASE WHEN EXISTS(SELECT * FROM INSERTED) THEN 'U' ELSE 'D' END)
+    END
+
+    DECLARE
+        @VAR1   VARCHAR(10),
+        @VAR2   VARCHAR(10),
+        @VAR3   VARCHAR(10),
+        @VAR4   VARCHAR(10),
+        @VAR5   VARCHAR(10)
+
+    IF @ACTION = 'D'
+    BEGIN
+        SELECT
+            @VAR1 = VAR1,
+            @VAR2 = VAR2,
+            @VAR3 = VAR3,
+            @VAR4 = VAR4,
+            @VAR5 = VAR5
+        FROM DELETED
+    END
+    ELSE
+    BEGIN
+        SELECT
+            @VAR1 = VAR1,
+            @VAR2 = VAR2,
+            @VAR3 = VAR3,
+            @VAR4 = VAR4,
+            @VAR5 = VAR5
+        FROM INSERTED
+    END
+
+    INSERT INTO LOG_TABLE (VAR1,VAR2,VAR3,VAR4,VAR5,TRI_TYPE,TRI_DTS)
+        VALUES (@VAR1,@VAR2,@VAR3,@VAR4,@VAR5,@ACTION,GETDATE())
+
+END
+GO
